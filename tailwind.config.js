57f16bcb57e4af976316bb7b0f3150e3512a470e
Alwaz <alwazkazi3@gmail.com>
2023-12-15 22:19:22 +0500

    chore: added color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        orange_primary: "#e9ab53",
+        red_primary: "#f15e50",
+        off_white: "#fffdfa",
+        grayish_blue: "#c5c6ce",
+        dark_grayish_blue: "#5d5f79",
+        very_dark_blue: "#00001a",
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         inter: ["Inter", "sans-serif"],
